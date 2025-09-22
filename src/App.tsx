75beedfce876929bde078a5673fe44ae0a67b044
Brazil Repo SA <dosamigosgpt@gmail.com>
2025-09-22 17:33:20 -0300

    Add existing ideas overview page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Dashboard } from "./components/Dashboard";
+import { NewIdea } from "./pages/NewIdea";
+import { ExistingIdeas } from "./pages/ExistingIdeas";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +18,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Index />}>
+            <Route index element={<Dashboard />} />
+            <Route path="nova-ideia" element={<NewIdea />} />
+            <Route path="ideias-existentes" element={<ExistingIdeas />} />
+          </Route>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
