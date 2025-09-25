f961412b5075706309be5df83e08790f3fa7af5a
Brazil Repo SA <dosamigosgpt@gmail.com>
2025-09-25 16:21:53 -0300

    Revert "Add Canvas section to sidebar navigation"

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ import { MyIdeas } from "./pages/MyIdeas";
 import { MarketMapping } from "./pages/MarketMapping";
 import { PainMapping } from "./pages/PainMapping";
 import { HypothesesGeneration } from "./pages/HypothesesGeneration";
-import { BusinessCanvas } from "./pages/BusinessCanvas";
-import { StrategyCanvas } from "./pages/StrategyCanvas";
 
 const queryClient = new QueryClient();
 
@@ -32,8 +30,6 @@ const App = () => (
             <Route path="mapeamento-mercado" element={<MarketMapping />} />
             <Route path="mapeamento-dores" element={<PainMapping />} />
             <Route path="geracao-hipoteses" element={<HypothesesGeneration />} />
-            <Route path="business-canvas" element={<BusinessCanvas />} />
-            <Route path="strategy-canvas" element={<StrategyCanvas />} />
           </Route>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
